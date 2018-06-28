c475c0798ba2534429e3d26e2808d61dc4ea7bc9
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-06-28 16:37:27 +0900

    종료 버튼 및 사칙연산 추가(예외처리 x)

--- src/main/java/com/example/rlawl/myapplication/MainActivity.java
+++ src/main/java/com/example/rlawl/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.rlawl.myapplication;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("정말로 종료하시겠습니까?");
+                builder.setTitle("종료 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("종료 알림창");
+                alert.show();
+            }
+        });
+    }
+
+    public void addClick(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1+n2));
+    }
+
+    public void subtractClick(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1-n2));
+    }
+    public void multiplyClick(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1*n2));
+    }
+    public void divideClick(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1/n2));
     }
 }
