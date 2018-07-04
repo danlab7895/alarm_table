7beceea1ccf1eb0e1ac6dd0fc2c52c9a5bd732a2
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-07-04 16:18:50 +0900

    알림 생성 조건 추가

--- src/main/java/com/example/rlawl/myapplication/NotifyDemoActivity.java
+++ src/main/java/com/example/rlawl/myapplication/NotifyDemoActivity.java
@@ -83,6 +83,9 @@ public class NotifyDemoActivity extends AppCompatActivity {
         if(name!=null){
             builder.setContentTitle(name);
         }
+        else if(name == ""){
+            builder.setContentTitle("알림 생성");
+        }
         else{
             builder.setContentTitle("알림 생성");
         }
